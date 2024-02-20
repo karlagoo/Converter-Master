1b993c5aef8a94563e8dfe47fc363cc0eadacbf7
Karla Goo <karla_goo@yahoo.com>
2024-02-20 12:56:55 -0600

    added ids began function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function convert(){
-    console.log("Button Clicked")
+const textBox = document.getElementById("text_box");
+const fahrenheitTemp = document.getElementById("fahrenheit");
+const celciusTemp = document.getElementById("celcius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+    if (fahrenheitTemp.checked) {
+        //console.log("degrees in fahrenheit")
+        result.textContent = "You Selected Fahrenheit";
+    } else if (celciusTemp.checked) {
+       // console.log("degrees in celcius")
+        result.textContent = "You Selected Celcius";
+    } else {
+        result.textContent = "Select a unit";
+    }
 }
