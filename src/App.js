8ea93836cb217cf7fb25a3787109cc34eb2d7649
Karla Goo <karla_goo@yahoo.com>
2024-03-27 13:16:52 -0500

    Created reactapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Temperatures from './components/Temperatures'
+
+function App() {
+  return (
+    <div className="App">
+      <Temperatures />
+    </div>
+  );
+}
+
+export default App;
