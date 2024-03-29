3701d31f74a211791ba22260c3da9162364fe5c4
Karla Goo <karla_goo@yahoo.com>
2024-03-29 15:33:01 -0500

    changed calculations

--- src/components/temperatures/Temperatures.jsx
+++ src/components/temperatures/Temperatures.jsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import './temperatures.css'
 
 function Temperatures() {
+  const [metric, setMetric] = useState([]);
   const [input, setInput] = useState([]);
-  const fahrenheitTemp = document.getElementById("fahrenheit");
-  const celciusTemp = document.getElementById("celcius");
+  const [result, setResult] = useState([])
+
+  const handleRadioChange = (value) => {
+    setMetric(value)
+  }
 
   var convert = function () {
-    //console.log("button Clicked!")
-    if (fahrenheitTemp.checked) {
-      console.log('fahrenheit checked!')
+    if(metric === "fahrenheit"){
+      let temp = Number(input);
+      temp = temp * 9/5 + 32;
+
+    } else if (metric === "celcius") {
+      let temp = Number(input);
+      temp = (temp - 32) * (5 / 9);
     }
   };
 
@@ -19,20 +27,45 @@ function Temperatures() {
         <h1>Master Converter</h1>
         <h2 className="section_subtitle">Temperatures</h2>
         <div>
-          <input type="number" className="text_box" id="text_box" />
+          <input
+            type="number"
+            className="text_box"
+            id="text_box"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
           <br />
 
           <label htmlFor="fahrenheit">Celcius → Fahrenheit</label>
-          <input type="radio" name="temp_select" id="fahrenheit" />
+          <input
+            type="radio"
+            name="temp_select"
+            id="fahrenheit"
+            value={"fahrenheit"}
+            checked={metric === "fahrenheit"}
+            onChange={() => handleRadioChange("fahrenheit")}
+          />
           <br />
 
           <label htmlFor="celcius">Fahrenheit → Celcius</label>
-          <input type="radio" name="temp_select" id="celcius" />
+          <input
+            type="radio"
+            name="temp_select"
+            id="celcius"
+            value={"celcius"}
+            checked={metric === "celcius"}
+            onChange={() => handleRadioChange("celcius")}
+          />
           <br />
 
           <button type="button" onClick={convert}>
             Submit
           </button>
+
+          <p
+            id="result"
+            className="result_box"
+          >{result}</p>
         </div>
       </form>
     </div>
