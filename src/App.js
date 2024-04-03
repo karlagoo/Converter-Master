a3d9777aeef22efca5b5544a00d5de0e7b622c38
Karla Goo <karla_goo@yahoo.com>
2024-04-03 12:02:42 -0500

    init routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-import Temperatures from './components/temperatures/Temperatures'
-
+import Temperatures from './components/temperatures/Temperatures';
+import Currency from './components/currency/Currency';
+import TimeZone from './components/time/TimeZone';
 function App() {
   return (
     <div className="App">
       <form action="">
-        <h1>Master Converter</h1>
-        <h3>Currency | Temperatures | Time Zone
-        </h3>
-         <Temperatures />
+          <h1>Master Converter</h1>
+         <Router>
+          <div>
+            <li>
+              <Link to='/currency'>Currency</Link>
+            </li>
+            <li>
+              <Link to='/temperatures'>Temperatures</Link>
+            </li>
+            <li>
+              <Link to='/time_zone'>Time Zone</Link>
+            </li>
+          </div>
+         </Router>
+          
       </form>
-     
+
     </div>
   );
 }
