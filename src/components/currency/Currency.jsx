5c4e230283ca15cfa41d400f21da5cd0e2e4d308
Karla Goo <karla_goo@yahoo.com>
2024-04-10 01:05:19 -0500

    test api

--- src/components/currency/Currency.jsx
+++ src/components/currency/Currency.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CurrencyRow from "./CurrencyRow";
 
+const BASE_URL =
+  "https://v6.exchangerate-api.com/v6/656b9ae6e8d9526066234d8b/latest/USD";
+
 function Currency() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions)
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.base_code)
+    })
+  }, [])
+
   return (
     <div>
       <h2>Currency</h2>
       <CurrencyRow />
-      <div>=</div>
+      <div className="equals">=</div>
       <CurrencyRow />
     </div>
   );
